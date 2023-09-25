ac92d8702d40ae6ed81006992248c65accaf58b1
витя Полищук <polishchuk.viktor1310@gmail.com>
2023-09-25 10:47:52 +0300

    Добавьте файлы проекта.

---
+++ MyStack/MyStackCollection.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    public class MyStackCollection<T> : IEnumerable<T>, ICollection
+    {
+        private int _count;
+
+        private StackNode<T>? _first;
+        public int Count { get { return _count; } }
+        public StackNode<T>? First { get { return _first; } }
+
+        public object SyncRoot => this;
+
+        public bool IsSynchronized => false;
+        public MyStackCollection(IEnumerable<T> collection)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection), "Collection is empty");
+            foreach (var item in collection)
+            {
+                Push(item);
+            }
+        }
+        public MyStackCollection()
+        {
+            _count = 0;
+            _first = null;
+        }
+        public void Push(T item)
+        {
+            StackNode<T> nodePush = new StackNode<T>(item);
+            nodePush.NextElement = _first;
+            _first = nodePush;
+            _count++;
+        }
+        public T Pop()
+        {
+            if (_count == 0)
+                throw new InvalidOperationException("The stack is empty.");
+            T item = _first!.Data;
+            _first = _first.NextElement;
+            _count--; 
+            return item;
+        }
+        public T Peek()
+        {
+            if (_count == 0)
+                throw new InvalidOperationException("The stack is empty.");
+            return _first!.Data;
+        }
+        public void CopyTo(Array array, int index)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array), "Array is null");
+            if (index < 0 || index >= array.Length) throw new ArgumentOutOfRangeException(nameof(index));
+            if (this.Count > array.Length - index) throw new ArgumentException(nameof(this.Count));
+
+            StackNode<T>? current = _first;
+
+            while (current != null)
+            {
+                array.SetValue(current, index++);
+                current = current.NextElement;
+            }
+        }
+        public bool Contains(T item)
+        {
+            StackNode<T> stackNode = _first!;
+            while (stackNode != null)
+            {
+                if (stackNode.Data != null && stackNode.Data!.Equals(item))
+                    return true;
+                if (item == null && stackNode.Data == null)
+                    return true;
+                stackNode = stackNode.NextElement!;
+            }
+            return false;
+        }
+        public bool TryPeek(out T result)
+        {
+            if (_count != 0)
+            {
+                result = _first!.Data;
+                return true;
+            }
+            result = default!;
+            return false;
+        }
+        public bool TryPop(out T result)
+        {
+            if (_count != 0)
+            {
+                result = this.Pop();
+                return true;
+            }
+            result = default!;
+            return false;
+        }
+        public void Clear()
+        {
+            while (this.Count != 0)
+            {
+                this.Pop();
+            }
+            _count = 0;
+        }
+       
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyEnumerator(this);
+            /* StackNode<T> stackNode= _first;
+           while (stackNode != null)
+           {
+               yield return stackNode.Value;
+               stackNode = stackNode.NextElement;
+
+           }*/
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        private sealed class MyEnumerator : IEnumerator<T>
+        {
+            private StackNode<T>? _current;
+
+            private readonly MyStackCollection<T>? _stack;
+            object IEnumerator.Current => Current!;
+            public MyEnumerator(MyStackCollection<T> stack)
+            {
+                _stack = stack;
+                if (_stack is not null && _stack.Count > 0)
+                {
+                    _current = default;
+                }
+            }
+            public T Current
+            {
+                get
+                {
+                    if (_current == null)
+                        throw new InvalidOperationException();
+                    return _current.Data;
+                }
+
+            }
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                if (_current == null)
+                {
+                    _current = _stack!.First;
+                }
+                else
+                    _current = _current.NextElement;
+                return _current != null;
+            }
+
+            public void Reset()
+            {
+                _current = default;
+            }
+        }
+
+    }
+}
---
+++ MyStack/StackNode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MyStack
+{
+    public class StackNode<T>
+    {
+        public T Data { get; set; }
+        public StackNode<T>? NextElement { get; set; }
+        public StackNode(T data)
+        {
+            Data = data;
+            NextElement = default;
+        }
+    }
+}
