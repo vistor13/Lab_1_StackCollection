3cd78c0ab2a25da03a203a665f7c11555e570e90
витя Полищук <polishchuk.viktor1310@gmail.com>
2023-09-25 17:08:03 +0300

    add class EventHandler

---
+++ MyStack/StackEventHandler.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MyStack
+{
+    public class StackEventHandler<T>
+    {
+        public void Subscribe(MyStackCollection<T> stack)
+        {
+            stack.ItemPushed += AddedEvent!;
+            stack.ItemPopped += AddedEvent!;
+        }
+
+        public void Unsubscribe(MyStackCollection<T> stack)
+        {
+            stack.ItemPopped -= RemovedEvent!;
+            stack.ItemPopped -= RemovedEvent!;
+        }
+
+        private void AddedEvent(object sender, StackEventArg<T> e)
+        {
+            if (sender == null) return;
+            Console.WriteLine("Item was added: " + e.Value + "; " + e.Time);
+        }
+
+        private void RemovedEvent(object sender, StackEventArg<T> e)
+        {
+            if (sender == null) return;
+            Console.WriteLine("Item was removed: " + e.Value + "; " + e.Time);
+        }
+    }
+}
